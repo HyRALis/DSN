2c503191ccc4812835b8bebbac38e4946a86677a
HyRALis <petar.trajanoski.pt@gmail.com>
2020-07-12 23:57:25 +0200

    Registering  User (encripting pw, gravatar etc.) & generating JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
-// @route POST api/users
-// @desc  Test route
-//@access Public
+const User = require("../../models/User");
+
+// @route    POST api/users
+// @desc     Test route
+// @access   Public
 
 router.post(
   "/",
@@ -13,15 +19,61 @@ router.post(
     check("email", "Please enter a valid email").isEmail(),
     check(
       "password",
-      "Please eneter a password with 8 or more characters"
+      "Please enter a password with 8 or more characters"
     ).isLength({ min: 8 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User Route");
+
+    const { name, email, password } = req.body;
+    try {
+      //User exist check
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+      //Get User gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+      //encript pw
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      //return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          return res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Problem");
+    }
   }
 );
 
--- server.js
+++ server.js
@@ -9,7 +9,6 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 //Define Routes
-app.get("/", (req, res) => res.send("API running"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/auth", require("./routes/api/auth"));
